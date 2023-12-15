aa4257dc1bcc10ded25950172aff34c89e55267a
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-12-15 05:54:40 +0300

    feat: changed package name to sound-streamlit & updated keywords

--- setup.py
+++ setup.py
@@ -14,20 +14,26 @@ with open("README.md", "r") as f:
 
 
 setup(
-    name="streamlit-audio-widgets",
-    version="0.1.0",
+    name="sound-streamlit",
+    version="0.1.2",
     author="YeaMerci",
     author_email="entertomerci@gmail.com",
     description="The package contains a ready-to-use streamlit widget for downloading or recording audio. There is support for most audio formats.",
     long_description=long_description,
     install_requires=requirements,
     long_description_content_type="text/markdown",
-    url="https://github.com/YeaMerci/streamlit-audio-widgets",
+    url="https://github.com/YeaMerci/sound-streamlit",
     packages=find_packages(),
     include_package_data=True,
+    keywords=[
+        "waveform", "converting",
+        "streamlit-audio-recorder",
+        "recorder", "audio",
+        "streamlit", "streamlit-component"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10"
