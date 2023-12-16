4eb2f62264edd8b95cbbfbac51faa4dfaa425bfc
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-12-16 13:32:22 +0300

    style: added component build & src

---
+++ soundlit/recorder/frontend/build/precache-manifest.83caf07c04aae44ab28c59271a26f4ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33418bafe8a2ed0d493c3780add57f3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "87355113e4a7155e7166",
+    "url": "./static/css/2.bfbf028b.chunk.css"
+  },
+  {
+    "revision": "87355113e4a7155e7166",
+    "url": "./static/js/2.6b4a2cce.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.6b4a2cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb04a95ed5555ec7248",
+    "url": "./static/js/main.3c8905ed.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
