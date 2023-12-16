31e199ce2883d94bc86733f5e8eacd1b9c2aa2bd
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-12-16 13:33:23 +0300

    ref: soundlit

--- soundlit/recorder/__init__.py
+++ soundlit/recorder/__init__.py
@@ -3,16 +3,22 @@ import numpy as np
 import streamlit as st
 from io import BytesIO
 import streamlit.components.v1 as components
+from streamlit.components.v1.components import CustomComponent
+
+
+def _build_component() -> CustomComponent:
+    """
+    Get parent directory relative to current directory.
+    Custom REACT-based component for recording client audio in browser.
+    Specify directory and initialize st_audiorec object functionality.
+    """
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    return components.declare_component("recorder", path=build_dir)
 
 
 def st_audiorec():
-    # get parent directory relative to current directory
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    # Custom REACT-based component for recording client audio in browser
-    build_dir = os.path.join(parent_dir, "frontend/build")
-    # specify directory and initialize st_audiorec object functionality
-    st_audiorec = components.declare_component("st_audiorec", path=build_dir)
-
+    st_audiorec = _build_component()
     # Create an instance of the component: STREAMLIT AUDIO RECORDER
     raw_audio_data = st_audiorec()  # raw_audio_data: stores all the data returned from the streamlit frontend
     wav_bytes = None                # wav_bytes: contains the recorded audio in .WAV format after conversion
@@ -21,8 +27,8 @@ def st_audiorec():
     # (this arraybuffer is derived from web-media API WAV-blob data)
 
     if isinstance(raw_audio_data, dict):  # retrieve audio data
-        with st.spinner('retrieving audio-recording...'):
-            ind, raw_audio_data = zip(*raw_audio_data['arr'].items())
+        with st.spinner("retrieving audio-recording..."):
+            ind, raw_audio_data = zip(*raw_audio_data["arr"].items())
             ind = np.array(ind, dtype=int)  # convert to np array
             raw_audio_data = np.array(raw_audio_data)  # convert to np array
             sorted_ints = raw_audio_data[ind]
